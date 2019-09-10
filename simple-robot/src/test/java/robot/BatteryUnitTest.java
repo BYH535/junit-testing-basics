5862ba34467b92380b4eda6a0cb4f072e44e80b3
Hamza BEN YAHIA <hamza.by3@gmail.com>
2019-09-10 14:30:26 +0200

    Adding source code

---
+++ simple-robot/src/test/java/robot/BatteryUnitTest.java
@@ -0,0 +1,17 @@
+package robot;
+
+import junit.framework.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fabrice
+ * Date: 26/08/13
+ * Time: 14:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class BatteryUnitTest {
+
+
+}
